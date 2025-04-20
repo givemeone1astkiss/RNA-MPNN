1a61ee19f0be2f864fa64a80a9aabf536ace4e6f
givemeone1astkiss <2210307323@stu.pku.edu.cn>
2025-04-20 18:52:39 +0800

    Update predictor.

--- rnampnn/utils/predict.py
+++ rnampnn/utils/predict.py
@@ -6,11 +6,11 @@ import torch
 from tqdm import tqdm
 
 
-def predict(model: RNAModel, data_path=COMPETITION_DATA, output_path=COMPETITION_OUT):
+def predict(model: RNAModel, batch_size=32, data_path=COMPETITION_DATA, output_path=COMPETITION_OUT):
     
     gen_dataframe(file_path=f'{data_path}seqs/').to_csv(f'{data_path}predict_data.csv', index=False)
     predict_dataset = RNADataset(f'{data_path}predict_data.csv', f'{data_path}coords')
-    data = torch.utils.data.DataLoader(predict_dataset, batch_size=32, num_workers=19, shuffle=False, persistent_workers=True, collate_fn=featurize)
+    data = torch.utils.data.DataLoader(predict_dataset, batch_size, num_workers=19, shuffle=False, persistent_workers=True, collate_fn=featurize)
     for batch_id, batch in tqdm(enumerate(data), total=len(data), desc="Predicting", unit="batch"):
         model.predict(batch, batch_id, output_dir=output_path)
     print(f"Predictions saved to {output_path}")
