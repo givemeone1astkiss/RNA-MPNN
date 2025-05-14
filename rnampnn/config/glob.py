de5d4a3ef965b405fc16d0e8c542b0ba45bd31b8
givemeone1astkiss <2210307323@stu.pku.edu.cn>
2025-05-15 00:53:30 +0800

    Upload RNAMPNN-AF V5

--- rnampnn/config/glob.py
+++ rnampnn/config/glob.py
@@ -20,4 +20,4 @@ DEFAULT_SEED = 42
 VOCAB = {'A': 0, 'U': 1, 'C': 2, 'G': 3}
 REVERSE_VOCAB = {0: 'A', 1: 'U', 2: 'C', 3: 'G'}
 
-BEST_CKPT: str = 'out/checkpoints/RNAMPNN-AF/checkpoint-epoch=355-4.ckpt'
+BEST_CKPT: str = 'out/checkpoints/RNAMPNN-AF/checkpoint-epoch=470-5.ckpt'
